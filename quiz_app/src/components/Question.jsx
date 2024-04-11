d0ab3fc1c63fc296ee0b17cf66a690827bb1c4c5
jerenteria <renteria.juan96@gmail.com>
2024-04-10 18:50:13 -0700

    more errors

--- quiz_app/src/components/Question.jsx
+++ quiz_app/src/components/Question.jsx
@@ -1,26 +1,51 @@
 import QuestionTimer from "./QuestionTimer.jsx";
 import Answers from "./Answers.jsx";
+import { useState } from 'react';
+import QUESTIONS from '../questions.js';
 
 export default function Question({
-  questionText,
-  answers,
   onSelectAnswer,
-  selectedAnswer,
-  answerState,
   onSkipAnswer,
+  key
 }) {
 
+    const [answer, setAnswer] = useState({
+        selectedAnswer = '',
+        isCorrect: null
+    });
     
-    
+    function handleSelectAnswer(answer) {
+        setAnswer({
+            selectedAnswer: answer, // set answer to answer selected(answer being taken in)
+            isCorrect: null 
+        })
+        setTimeout(() => {
+            setAnswer({
+            selectedAnswer: answer,
+            isCorrect: QUESTIONS[key].answers[0] === answer
+        })
+        // set timer to show user answer for 2 seconds    
+        setTimeout(() => {
+            onSelectAnswer(answer);
+        }, 2000);
+        },1000)
+    }
+
+    let answerState = '';
+
+    if(answer.selectedAnswer) {
+        answerState = answer.isCorrect ? 'correct' : 'wrong';
+    }
+
   return (
     <div id="question">
       <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[key].text}</h2>
       <Answers
-        answers={answers} // render available answers not shuffled answers
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[key].answers} // render available answers not shuffled answers
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer} 
+        onSelect={handleSelectAnswer} 
       />
     </div>
   );
